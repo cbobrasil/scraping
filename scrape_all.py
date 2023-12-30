3c1b3e2d6da01d9092548f3355b53a7ea47eee7f
cbobrasil <cbobrasil@gmail.com>
2023-12-30 11:56:24 +0000

    scraping books

--- scrape_all.py
+++ scrape_all.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import psycopg2
 
-# Função para obter informações de uma página
+# Function to get information from one page 
 def scrape_page(url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -12,26 +12,28 @@ def scrape_page(url):
         book_data = []
         for book in books:
             title = book.h3.a["title"]
-            price = book.find("p", class_="price_color").get_text().strip()
+            price_with_currency = book.find("p", class_="price_color").get_text().strip()
+            price_digits = ''.join(char for char in price_with_currency if char.isdigit() or char == '.')
+            price = float(price_digits)
             rating = book.p["class"][1]
             book_data.append((title, price, rating))
         return book_data
     return []
 
-# Conectar ao banco de dados
+# Conecting to postgres database
 connection = psycopg2.connect(host='localhost', user='postgres', password='senha', dbname='postgres')
 cur = connection.cursor()
 
-# Iterar sobre as páginas de 1 a 50
+# Scrap until page 50
 for page_num in range(1, 51):
     url = f'https://books.toscrape.com/catalogue/page-{page_num}.html'
     page_data = scrape_page(url)
     
-    # Inserir informações no banco de dados
+    # Insert information into the db 
     for data in page_data:
         cur.execute("""INSERT INTO books (title, price, rating) VALUES (%s, %s, %s)""", data)
         connection.commit()
 
-# Fechar a conexão
+# Close conection 
 cur.close()
 connection.close()
